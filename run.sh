d175123e23cdd833fe844d01e976aeb84fda8ae4
Husam <cto@escoiq.com>
2024-06-07 21:00:11 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -16,4 +16,4 @@ sed -i 's/20016/'$CHAT'/g' $DESTINATION/docker-compose.yml
 # run Odoo
 docker-compose -f $DESTINATION/docker-compose.yml up -d
 
-echo 'Started Odoo @ http://localhost:'$PORT' | Master Password: Escoiq.com | Live chat port: '$CHAT
+echo 'Started Odoo @ http://localhost:'$PORT' | Master Password: Escoiq@2024 | Live chat port: '$CHAT
